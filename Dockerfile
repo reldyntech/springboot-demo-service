e6061e13d6d021215c2365c2f92ecdb6c4448402
Jaya Prasad Rao <prasad@reldyn.co>
2020-05-06 14:02:08 +0800

    Final commit

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM openjdk:8-jdk-alpine
 RUN apk add --no-cache tzdata
 ENV TZ Asia/Kuala_Lumpur
 VOLUME /tmp
-ARG JAR_FILE=target/springboot-demo-service-0.0.1-SNAPSHOT.jar
+ARG JAR_FILE=target/springboot-demo-service-1.0.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT exec java $JAVA_OPTS -Xverify:none -Djava.security.egd=file:/dev/./urandom -jar /app.jar
--- src/main/java/co/reldyn/service/springbootdemoservice/controllers/DemoController.java
+++ src/main/java/co/reldyn/service/springbootdemoservice/controllers/DemoController.java
@@ -3,16 +3,38 @@ package co.reldyn.service.springbootdemoservice.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class DemoController {
 
+    /**
+     * Welcome
+     */
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to Alibaba Cloud Tech Meet 2020!";
     }
 
-//    @GetMapping("/hello")
-//    public String hello() {
-//        return "Hello World!";
-//    }
+
+    /**
+     * Get the host networking information
+     */
+    // @GetMapping("/netinfo")
+    // public Map<String, String> netinfo() {
+    //     Map<String, String> map = new HashMap<>();
+    //     try {
+    //         InetAddress inetAddress = InetAddress.getLocalHost();
+    //         map.put("message", "Welcome to Alibaba TechMeet 2020");
+    //         map.put("hostname", inetAddress.getHostName());
+    //         map.put("ipAddress", inetAddress.getHostAddress());
+    //         return map;
+    //     } catch (UnknownHostException e) {
+    //         e.printStackTrace();
+    //         return map;
+    //     }
+    // }
 }
